c9a9d72e58e4a5eb34c585fb7c749de2a39aaca1
matinf24 <a@b.c>
2025-01-21 15:43:05 +0100

    Ikonki

--- classes/simulation.py
+++ classes/simulation.py
@@ -54,7 +54,7 @@ class Simulation:
             pygame.transform.scale(pygame.image.load('img/win98.png'), (32, 32)),
             pygame.transform.scale(pygame.image.load('img/MyComputer98.png'), (32, 32)),
             pygame.transform.scale(pygame.image.load('img/InternetExplorer98.png'), (24, 24)) , # Ensure icons are the same size
-            pygame.transform.scale(pygame.image.load('img/InternetExplorer98.png'), (24, 24))
+            pygame.transform.scale(pygame.image.load('img/Calc.png'), (24, 24))
             # Ensure icons are the same size
         ]
 
