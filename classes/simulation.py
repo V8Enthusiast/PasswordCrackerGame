c3b990e9effeb32e0549af9622bcbf2d7b617f01
matinf24 <qewf2QEF@.COM>
2025-01-21 11:30:48 +0100

    Calc

--- classes/simulation.py
+++ classes/simulation.py
@@ -435,10 +435,17 @@ class Calculator(Window):
         self.y=y
 
         self.current_text_rect.center=(self.x//2,self.y//2)
+    def update_string(self):
+        self.current_text = self.font2.render(self.current_string, True, (255, 255, 255))
+        self.current_text_rect = self.current_text.get_rect()
+        self.current_text_rect.center = (self.x // 2, self.y // 2)
     def draw(self,screen):
         super().draw(screen)
-        self.surface.blit(self.current_text,self.current_text_rect)
+        self.surface.fill((0,0,0))
+        self.surface.blit(self.current_text, self.current_text_rect)
+        self.shift=False
     def handle_event(self, event):
+
         if event.type == pygame.MOUSEBUTTONDOWN:
             if self.draggable_area().collidepoint(event.pos):
                 self.dragging = True
@@ -470,6 +477,39 @@ class Calculator(Window):
             if self.dragging:
                 self.rect.x = event.pos[0] + self.offset_x
                 self.rect.y = event.pos[1] + self.offset_y
+
+
+        #     dict={pygame.K_0:("0",")"),pygame.K_1:("1",""),pygame.K_2:("2",""),pygame.K_3:("3",""),pygame.K_4:("4",""),pygame.K_5:("5",""),pygame.K_6:("6",""),pygame.K_7:("7",""),pygame.K_8:("8","*"),pygame.K_9:("9","("),pygame.K_SPACE:(" ",""),pygame.K_EQUALS:("","+"),pygame.K_MINUS:("-",""),pygame.K_SLASH:(":",""),pygame.K_SEMICOLON:("",":")}
+        #
+        #     e=event.key
+        #     print(e)
+        #     if e == pygame.K_LSHIFT or e == pygame.K_RSHIFT:
+        #         self.shift = True
+        #     for x in dict.keys():
+        #         if e==x:
+        #             if self.shift:
+        #                 self.current_string+=dict[e][1]
+        #             else:
+        #
+        #                 self.current_string += dict[e][0]
+        #             self.update_string()
+        # elif event.type==pygame.KEYUP:
+        #     if event.key==pygame.K_LSHIFT or event.key==pygame.K_RSHIFT:
+        #         self.shift=False\
+
+        elif event.type == pygame.KEYDOWN:
+            if self.active:
+                if event.key == pygame.K_RETURN:
+                    self.current_string=str(eval(self.current_string))
+                    # eval(f" = {}")
+                    print(self.current_string)
+                elif event.key == pygame.K_BACKSPACE:
+                    self.current_string = self.current_string[:-1]
+                else:
+                    self.current_string += event.unicode
+                self.update_string()
+
+
 
 class Minesweeper(Window):
     def draw(self, screen):
