68fdc169a909d782cb4a2183ec0dc44cc9fb005c
chris <christopher.knap@gmail.com>
2025-02-12 17:02:32 +0100

    better gameover screen

--- classes/gameover.py
+++ classes/gameover.py
@@ -1,56 +1,98 @@
 import pygame
 from classes import buttons
 
+
 class GameOver:
     def __init__(self, app, score):
         self.app = app
         self.debug = False
 
-        self.main_text_rect_center = (self.app.width // 2, 250 * self.app.scale)
-        self.score_text_rect_center = (self.app.width // 2,750 * self.app.scale)
-        self.font_small = pygame.font.Font("fonts/Windows98.ttf", 36)
-        self.font_smaller = pygame.font.Font("fonts/Windows98.ttf", 24)
-        self.font = pygame.font.Font("fonts/Windows98.ttf", 72)
-        self.font_color = (192, 192, 192)
-        self.bg_color = (0, 0, 128)
+        # Windows 98 BSOD colors
+        self.BSOD_BLUE = (0, 0, 170)
+        self.BSOD_WHITE = (255, 255, 255)
+
+        # Screen positioning
+        self.margin_left = 50 * self.app.scale
+        self.line_spacing = 40 * self.app.scale
+        self.start_y = 100 * self.app.scale
+
+        # Font setup for that authentic look
+        self.font = pygame.font.Font("fonts/Windows98.ttf", int(28 * self.app.scale))
+        self.font_bold = pygame.font.Font("fonts/Windows98.ttf", int(32 * self.app.scale))
+
+        # Button setup
         self.buttons = [
-            buttons.Button(200 * self.app.scale, 75 * self.app.scale, self.app.width / 2 - 100 * self.app.scale,
-                           self.app.height / 2 - 75 * self.app.scale / 2, self.font_smaller, "Play again", 'play',self.app,(0, 0, 0),
-                           self.font_color)]
+            buttons.Button(
+                200 * self.app.scale,
+                75 * self.app.scale,
+                self.app.width / 2 - 100 * self.app.scale,
+                self.app.height - 150 * self.app.scale,
+                self.font,
+                "Reboot",
+                'play',
+                self.app,
+                self.BSOD_BLUE,
+                self.BSOD_WHITE
+            )
+        ]
+
         self.window = self.app.screen
         self.score = score
 
+        # Format time for display
         self.minutes = str(int(self.score) // 60)
         self.seconds = str(int(self.score) % 60)
 
-        if self.app.inactive_simulation.isFreeRAMDownloaded:
-            self.msg = "Free RAM... really?"
-        else:
-            self.msg = "You survived for " + self.minutes + " minutes and " + self.seconds + " seconds"
+        self.attack_types = {
+            "Dictionary attack" : "This type of attack uses databases of known words or passwords to find tre correct one",
+            "Bruteforce attack" : "This type of attack relies on generating every possible combination of characters to break the users password",
+            "Number attack" : "This type of attack works just like bruteforce, with the difference that it only generates sequences of numbers, which makes it much faster",
+            "UNKNOWN": "UNKNOWN"
+        }
 
-        self.attack_type = ""
+        self.attack_help = {
+            "Dictionary attack" : "To prevent this attack, try making your password more unique, add numbers and special characters or try not using words at all",
+            "Bruteforce attack" : "This attack is inevitable, you can't do much to stop it except making your password longer",
+            "Number attack" : "Try using a combination of letters and numbers instead of just digits",
+            "UNKNOWN" : "UNKNOWN"
+        }
 
-        if self.app.inactive_simulation.hack_method != "":
-            self.attack_type = self.app.inactive_simulation.hack_method
+        self.attack = self.app.inactive_simulation.hack_method if self.app.inactive_simulation.hack_method else 'UNKNOWN'
+
+        # Prepare error messages
+        self.error_text = [
+            "A fatal exception 0E has occurred at 0028:C0011E36 in VXD VMM(01) +",
+            "00010E36. The current process will be terminated.",
+            "",
+            f"* Time survived before crash: {self.minutes}m {self.seconds}s",
+            f"* Attack method: {self.attack}",
+            f"* How it works: {self.attack_types[self.attack]}",
+            f"* How to prevent it: {self.attack_help[self.attack]}",
+            "",
+            "SYSTEM HALTED"
+        ]
 
         self.app.newSimulation()
 
-        
     def render(self):
-        self.window.fill(self.bg_color)
+        # Fill screen with BSOD blue
+        self.window.fill(self.BSOD_BLUE)
+
+        # Render main error header
+        header = self.font_bold.render("Windows", True, self.BSOD_WHITE)
+        self.window.blit(header, (self.margin_left, self.start_y))
+
+        # Render all error messages
+        current_y = self.start_y + self.line_spacing * 2
+        for line in self.error_text:
+            text_surface = self.font.render(line, True, self.BSOD_WHITE)
+            self.window.blit(text_surface, (self.margin_left, current_y))
+            current_y += self.line_spacing
+
+        # Render button
         for button in self.buttons:
             button.render(self.app.screen)
 
-        display_text = self.font.render("You have been hacked!" + self.attack_type, True, self.font_color)
-        score_text = self.font_small.render(self.msg, True, self.font_color)
-
-        display_text_rect = display_text.get_rect()
-        score_text_rect = display_text.get_rect()
-        display_text_rect.center = self.main_text_rect_center
-        score_text_rect.center = self.score_text_rect_center
-        self.app.screen.blit(display_text, display_text_rect)
-        self.app.screen.blit(score_text, score_text_rect)
-
-    # Overrides the default events function in app.py
     def events(self):
+        # Handle any events if needed
         pass
